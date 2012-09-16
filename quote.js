3fd05e9b312e2786e27e5cae542aa83bdd86b02b
Marius <kofinc@gmail.com>
2012-09-16 19:14:02 +0200

    meh, stupid Marius, use .getJSON instead of dark XMLHttpRequest

--- quote.js
+++ quote.js
@@ -8,20 +8,13 @@
  * ----------------------------------------------------------------------------
  */
 
-var req = new XMLHttpRequest();
-            req.open("GET", "quote.json", true);
-            req.onreadystatechange = quoteParser;
-            req.send(null);
-            function quoteParser(){
-                if (req.readyState === 4) {
-                    var quote = eval('(' + req.responseText + ')');
-                    /* Take Random number between 0 & quote length */
-                    var i = quote.quotes.length;
-                    var nb = Math.floor(Math.random() * i);
-                    /* Assigne value to variable for easier display */
-                    var text = quote.quotes[nb].text;
-                    var author = quote.quotes[nb].author;
-                    /* Display the quote */
-                    $('.quote').html('<p>'+text+'</p><footer>'+author+'</footer>');
-                }
-            }
+$.getJSON('quote.json',function(data) {
+    /* Take Random number between 0 & quote length */
+    var i = data.quotes.length;
+    var nb = Math.floor(Math.random() * i);
+    /* Assigne value to variable for easier display */
+    var text = data.quotes[nb].text;
+    var author = data.quotes[nb].author;
+    /* Display the quote */
+    $('.quote').html('<p>'+text+'</p><footer>'+author+'</footer>');
+});
